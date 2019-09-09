335fab6ce18bdf035e4bcb56879f31493ffa35ca
kamilGawron <kgawron955@gmail.com>
2019-09-09 20:26:00 +0200

    Updates

---
+++ precache-manifest.67f061fe202b080ac3b356651d48e3ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3e44436e3e9ad5a4d49945169682cf",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "cfacfb12593b78cd512b",
+    "url": "/calculator/static/css/main.f3415631.chunk.css"
+  },
+  {
+    "revision": "d8a95c9f1cef34288fbe",
+    "url": "/calculator/static/js/2.1a59d3ef.chunk.js"
+  },
+  {
+    "revision": "cfacfb12593b78cd512b",
+    "url": "/calculator/static/js/main.5e2f8719.chunk.js"
+  },
+  {
+    "revision": "335217aced5d2de02c3f",
+    "url": "/calculator/static/js/runtime~main.7273a9b5.js"
+  }
+]);
